4bbbaa76019f25a56bf4e3ce249ed22840609fd1
nanfengovo <nanfengovo@gmail.com>
2025-04-10 22:57:34 +0800

    WPF-个性化页面开发完成

--- DailyAPP.WPF/DailyAPP.WPF/ViewModels/PersonalUCViewModel.cs
+++ DailyAPP.WPF/DailyAPP.WPF/ViewModels/PersonalUCViewModel.cs
@@ -1,15 +1,28 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using MaterialDesignColors;
+using MaterialDesignColors.ColorManipulation;
+using MaterialDesignThemes.Wpf;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace DailyAPP.WPF.ViewModels
 {
     class PersonalUCViewModel:BindableBase
     {
+        private readonly PaletteHelper paletteHelper = new();
+
+        public PersonalUCViewModel()
+        {
+            ChangeHueCommand = new DelegateCommand<object>(ChangeHue);
+        }
+
+        #region 设置主题背景
         private bool _isDarkTheme;
         public bool IsDarkTheme
         {
@@ -32,5 +45,27 @@ namespace DailyAPP.WPF.ViewModels
 
             paletteHelper.SetTheme(theme);
         }
+        #endregion
+
+
+        public IEnumerable<ISwatch> Swatches { get; } = SwatchHelper.Swatches;
+
+        #region 修改顶部的背景色
+        public DelegateCommand<object> ChangeHueCommand { get; }
+
+        private void ChangeHue(object? obj)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            var color = (Color)obj;
+            theme.PrimaryLight = new ColorPair(color.Lighten());
+            theme.PrimaryMid = new ColorPair(color);
+            theme.PrimaryDark = new ColorPair(color.Darken());
+
+            paletteHelper.SetTheme(theme);
+        }
+
+        #endregion
+
     }
 }
